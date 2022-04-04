bcda26ccb806cfcd15fb8a8e68792be2feb4110f
Adam El Amrani <adelamco1091@outlook.com>
2022-04-04 17:16:09 +0200

    Create layout and navbar

---
+++ components/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,16 @@
+const NavigationBar: React.FunctionComponent = (): JSX.Element => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>Pokémon logo</li>
+          <li>Future</li>
+          <li>Future</li>
+          <li>Future</li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default NavigationBar;
